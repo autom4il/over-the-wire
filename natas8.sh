2fb0b8cf0182c849e16656125786b958d6d2c972
autom4il <109908737+autom4il@users.noreply.github.com>
2023-01-04 15:17:59 +0100

    Create natas8.sh

---
+++ natas8.sh
@@ -0,0 +1,10 @@
+<<COMMENTS
+
+Author: autom4il
+Date: 04/01/2023
+Website: https://overthewire.org/wargames/natas/
+Challenge: Level 7 --> Level 8
+
+COMMENTS
+
+echo '3d3d516343746d4d6d6c315669563362' |xxd -r -p |rev |base64 -d |xargs echo "[+] The key is:"
